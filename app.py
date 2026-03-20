a5858b61fa915d4f4e492396e289a5d0763f25ce
JackPersonal <jtoenjes05@gmail.com>
2026-03-20 01:50:50 -0600

    accessible through vercel and local host

--- app.py
+++ app.py
@@ -6,8 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template("board.html"), 200
 
-@app.route("/board") #default
+@app.route("/board") 
 def board():
     return render_template("board.html"), 200
 
-# app.run("0.0.0.0", port=7652) 
+#for local host
+if __name__ == "__main__":
+    app.run("0.0.0.0", port=7652) 
